87569227893db747d5602102510abed2a4725b42
Leon-Loov <leon.loov@outlook.com>
2023-07-20 16:18:12 +0200

    Create API endpoint for creating goals

---
+++ src/app/api/create_goal/route.ts
@@ -0,0 +1,134 @@
+import { NextRequest } from "next/server";
+import { getSession, createResponse } from "@/lib/session"
+import prisma from "@/prismaClient";
+import { GoalInput } from "@/types";
+import { Prisma } from "@prisma/client";
+
+export async function POST(request: NextRequest) {
+  const response = new Response();
+  const session = await getSession(request, response);
+
+  let goal: GoalInput = await request.json();
+  goal.dataSeries
+
+  // Validate request body
+  if (!goal.name || !goal.goalObject || !goal.indicatorParameter) {
+    return createResponse(
+      response,
+      JSON.stringify({ message: 'Missing required input parameters' }),
+      { status: 400 }
+    );
+  }
+  /*
+  else if (goal.dataSeries && goal.dataSeriesId) {
+    return createResponse(
+      response,
+      JSON.stringify({
+        message: 'Cannot create a new data series and reference an existing one at the same time'
+      }),
+      { status: 400 }
+    );
+  }
+  */
+  goal.dataSeriesId = goal.dataSeriesId || "";
+
+  // Validate session
+  if (!session.user?.isLoggedIn) {
+    return createResponse(
+      response,
+      JSON.stringify({ message: 'Unauthorized' }),
+      { status: 401 }
+    );
+  }
+
+  // Create lists of UUIDs for linking
+  let editors: { id: string }[] = [];
+  for (let id of goal.editors || []) {
+    editors.push({ id: id });
+  }
+
+  let viewers: { id: string }[] = [];
+  for (let id of goal.viewers || []) {
+    viewers.push({ id: id });
+  }
+
+  let editGroups: { id: string }[] = [];
+  for (let id of goal.editGroups || []) {
+    editGroups.push({ id: id });
+  }
+
+  let viewGroups: { id: string }[] = [];
+  for (let id of goal.viewGroups || []) {
+    viewGroups.push({ id: id });
+  }
+
+  // Prepare for creating data series
+  let dataValues: Prisma.DataSeriesCreateWithoutGoalsInput = {
+    author: { connect: { id: session.user.id } },
+    unit: goal.dataUnit!,
+  };
+
+  if (goal.dataSeries?.length) {
+    let keys = Object.keys(dataValues);
+    // By selecting the keys containing numbers, we can assign data values to the correct fields in
+    // the database.
+    keys.filter(key => /\d/.test(key)).sort().forEach((key, index) => {
+      // This godawful mess assures TypeScript that we are not trying to assign numbers to any of the
+      // other fields in the dataSeries object.
+      dataValues[key as keyof Omit<Prisma.DataSeriesCreateWithoutGoalsInput,
+        'author' | 'unit' | 'id' | 'createdAt' | 'updatedAt' |
+        'editors' | 'viewers' | 'editGroups' | 'viewGroups'
+      >] = parseFloat(goal.dataSeries![index]);
+    });
+  }
+
+  // Create goal
+  try {
+    await prisma.goal.create({
+      data: {
+        name: goal.name,
+        goalObject: goal.goalObject,
+        nationalRoadmapId: goal.nationalRoadmapId || null,
+        indicatorParameter: goal.indicatorParameter,
+        author: {
+          connect: {
+            id: session.user.id,
+          },
+        },
+        editors: {
+          connect: editors,
+        },
+        viewers: {
+          connect: viewers,
+        },
+        editGroups: {
+          connect: editGroups,
+        },
+        viewGroups: {
+          connect: viewGroups,
+        },
+        dataSeries: {
+          connectOrCreate: {
+            where: {
+              id: goal.dataSeriesId,
+            },
+            create: dataValues,
+          }
+        },
+      }
+    });
+  } catch (e) {
+    console.log(e);
+    return createResponse(
+      response,
+      JSON.stringify({ message: "Internal server error" }),
+      { status: 500 }
+    );
+  }
+
+  return createResponse(
+    response,
+    JSON.stringify({ message: "Goal created successfully" }),
+    { status: 200 }
+  );
+}
---
+++ src/types.ts
@@ -0,0 +1,45 @@
+import { Prisma } from "@prisma/client";
+
+function exclude<Model, Key extends keyof Model>(
+  model: Model,
+  keys: Key[]
+): Omit<Model, Key> {
+  return Object.fromEntries(
+    Object.entries(model!).filter(([key]) => !keys.includes(key as Key))
+  ) as Omit<Model, Key>;
+}
+
+export type GoalInput = {
+  name: string;
+  goalObject: string;
+  // External IDs are UUIDs and thus strings, not numbers
+  nationalRoadmapId: string | null;
+  indicatorParameter: string;
+  // This will be turned into an actual dataSeries object by the API
+  // The expected input is a stringified array of floats
+  dataSeries: string[] | null;
+  // The unit of measurement for the data series, used when creating a new data series
+  dataUnit: string | null;
+  // In case the user wants to reference an existing data series instead of creating a new one
+  // TODO: Add a check to not allow both dataSeries and dataSeriesId to be set
+  dataSeriesId: string | null;
+  // Accepts lists of UUIDs for all of the following, to link them to the goal (optional)
+  editors: string[] | null;
+  viewers: string[] | null;
+  editGroups: string[] | null;
+  viewGroups: string[] | null;
+};
+
+const goalWithRelations = Prisma.validator<Prisma.GoalArgs>()({
+  include: {
+    dataSeries: true,
+    author: true,
+    editors: true,
+    viewers: true,
+    editGroups: true,
+    viewGroups: true,
+    actions: true,
+    roadmaps: true,
+  },
+});
+export type GoalWithRelations = Prisma.GoalGetPayload<typeof goalWithRelations>;
