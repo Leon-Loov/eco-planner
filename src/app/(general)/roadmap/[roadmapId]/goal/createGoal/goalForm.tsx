edfca061e944c9b1296d2884fa55a9e80092b7ba
Leon-Loov <leon.loov@outlook.com>
2023-09-25 16:48:36 +0200

    Update goal creation page

--- src/app/(general)/roadmap/[roadmapId]/goal/createGoal/goalForm.tsx
+++ src/app/(general)/roadmap/[roadmapId]/goal/createGoal/goalForm.tsx
@@ -9,6 +9,7 @@ export default function CreateGoal({ roadmapId, userGroups }: { roadmapId: strin
     event.preventDefault()
 
     const form = event.target as HTMLFormElement
+    console.log(form.dataSeries.value)
     const formJSON = JSON.stringify({
       name: form.goalName.value,
       goalObject: form.goalObject.value,
@@ -16,9 +17,11 @@ export default function CreateGoal({ roadmapId, userGroups }: { roadmapId: strin
       indicatorParameter: form.indicatorParameter.value,
       dataUnit: form.dataUnit.value,
       dataSeries: form.dataSeries.value,
-      dataSeriesId: form.dataSeriesId.value,
+      // This functionality is temporarily or permanently disabled
+      // dataSeriesId: form.dataSeriesId.value,
       roadmapId: roadmapId,
     })
+    console.log(formJSON)
 
     fetch('/api/createGoal', {
       method: 'POST',
@@ -37,15 +40,26 @@ export default function CreateGoal({ roadmapId, userGroups }: { roadmapId: strin
     })
   }
 
+  /**
+   * This matches 0 to 31 numbers separated by tabs or semicolons, with an optional decimal part.
+   * The first position represents the value for year 2020, and any number in the 31:st position represents the value for year 2050.
+   * 
+   * Two examle strings that match this pattern are:  
+   * "2.0;2.1;2.2;2.3;2.4;2.5;2.6;2.7;2.8;2.9;3.0;3.1;3.2;3.3;3.4;3.5;3.6;3.7;3.8;3.9;4.0;4.1;4.2;4.3;4.4;4.5;4.6;4.7;4.8;4.9;5.0"  
+   * and  
+   * ";0;;;4;1"
+   */
+  const dataSeriesPattern = "(([0-9]+([.,][0-9]+)?)?[\t;]){0,30}([0-9]+([.,][0-9]+)?)?"
+
   return (
     <>
       <form onSubmit={handleSubmit}>
         <button type="submit" disabled={true} style={{ display: 'none' }} aria-hidden={true} />
         <label htmlFor="goalName">Namn på målbanan: </label>
-        <input type="text" name="goalName" required={true} id="goalName" />
+        <input type="text" name="goalName" required id="goalName" />
         <br />
         <label htmlFor="goalObject">Målobjekt: </label>
-        <input type="text" name="goalObject" required={true} />
+        <input type="text" name="goalObject" required title="Målobjektet är den som &quot;äger&quot; ett mål, exempelvis en kommun, region eller organisation." id="goalObject" />
         <br />
         <label htmlFor="nationalRoadmapId">Nationell målbana denna är baserad på (om någon): </label>
         {/* TODO: Make this a dropdown with options from the database and proper IDs as values */}
@@ -58,18 +72,35 @@ export default function CreateGoal({ roadmapId, userGroups }: { roadmapId: strin
         <br />
         {/* TODO: Make this a dropdown with actual indicator parameters, plus a 'custom' option that allows typing in a custom parameter */}
         <label htmlFor="indicatorParameter">LEAP parameter: </label>
-        <select name="indicatorParameter" required={true} id="indicatorParameter">
+        <select name="indicatorParameter" required id="indicatorParameter">
+          <option value="">Välj indikatorparameter</option>
           <option value="Test">Indikatorparameter 1</option>
           <option value="Thing">Indikatorparameter 2</option>
           <option value="A\B">Indikatorparameter 3</option>
         </select>
         <br />
         <label htmlFor="dataUnit">Enhet för dataserie: </label>
-        <input type="text" name="dataUnit" required={true} id="dataUnit" />
+        <input type="text" name="dataUnit" required id="dataUnit" />
         <br />
+        <details>
+          <summary>
+            Extra information om dataserie
+          </summary>
+          <p>
+            Fältet &quot;Dataserie&quot; tar emot en serie värden separerade med semikolon eller tab, vilket innebär att du kan klistra in en serie värden från Excel eller liknande.<br />
+            <strong>OBS: Värden får inte vara separerade med komma (&quot;,&quot;).</strong><br />
+            Decimaltal kan använda antingen decimalpunkt eller decimalkomma.<br />
+            Det första värdet representerar år 2020 och serien kan fortsätta maximalt till år 2050 (totalt 31 värden).<br />
+            Om värden saknas för ett år kan du lämna det tomt, exempelvis kan &quot;;1;;;;5&quot; användas för att ange värdena 1 och 5 för år 2021 och 2025.
+          </p>
+        </details>
         <label htmlFor="dataSeries">Dataserie: </label>
         {/* TODO: Make this allow .csv files, and do parsing stuff and forced format if text */}
-        <input type="text" name="dataSeries" required={false} id="dataSeries" />
+        <input type="text" name="dataSeries" required id="dataSeries"
+          pattern={dataSeriesPattern}
+          title="Använd numeriska värden separerade med semikolon eller tab. Decimaltal kan använda antingen punkt eller komma."
+          style={{ width: 'auto' }}
+        />
         <br />
         {/* This functionality is temporarily or permanently disabled */}
         {/* <label htmlFor="dataSeriesId">Alternativt, välj en dataserie från listan: </label> */}
@@ -84,15 +115,8 @@ export default function CreateGoal({ roadmapId, userGroups }: { roadmapId: strin
         <AccessSelector groupOptions={userGroups} />
         <br />
         <input type="submit" value="Skapa målbana" />
-        <Tooltip anchorSelect="#goalObject, label[for=goalObject]">
+        <Tooltip anchorSelect="#goalObject">
           Målobjektet är den som &quot;äger&quot; ett mål, exempelvis en kommun, region eller organisation.
-        </Tooltip>
-        <Tooltip anchorSelect="#dataSeries, label[for=dataSeries]">
-          Du kan klistra in en serie värden separerade med mellanslag (&quot; &quot;), semikolon (&quot;;&quot;) eller tab (&quot;&emsp;&quot;).<br />
-          <strong>OBS: Inklistrade värden får inte vara separerade med komma (&quot;,&quot;).</strong><br />
-          Decimaltal kan använda antingen decimalpunkt eller decimalkomma.<br />
-          Det första värdet representerar år 2020 och serien kan fortsätta maximalt till år 2050 (totalt 31 värden).<br />
-          Om värden saknas för ett år kan du lämna det tomt, exempelvis kan &quot;;1;;;;5&quot; användas för att ange värdena 1 och 5 för år 2021 och 2025.
         </Tooltip>
       </form>
     </>
--- src/app/(general)/roadmap/[roadmapId]/goal/createGoal/page.tsx
+++ src/app/(general)/roadmap/[roadmapId]/goal/createGoal/page.tsx
@@ -2,14 +2,16 @@ import getUserGroups from "@/functions/getUserGroups";
 import { getSessionData } from "@/lib/session";
 import { cookies } from "next/headers";
 import CreateGoal from "./goalForm";
+import getOneRoadmap from "@/functions/getOneRoadmap";
 
 export default async function Page({ params }: { params: { roadmapId: string } }) {
   let session = await getSessionData(cookies())
   let userGroups: string[] = [...(await getUserGroups(session.user?.id!)), 'Public']
+  let roadmap = await getOneRoadmap(params.roadmapId)
 
   return (
     <>
-      <h1>Skapa målbana</h1>
+      <h1>Skapa ny målbana {roadmap?.name ? `under "${roadmap.name}"` : ""}</h1>
       <CreateGoal roadmapId={params.roadmapId} userGroups={userGroups} />
     </>
   )
