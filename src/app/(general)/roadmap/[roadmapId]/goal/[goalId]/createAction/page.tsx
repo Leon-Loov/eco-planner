0a6270c8c0342593474d8d6c785fa5a7de725db6
Leon-Loov <leon.loov@outlook.com>
2023-09-25 17:01:33 +0200

    Update action creation page

--- src/app/(general)/roadmap/[roadmapId]/goal/[goalId]/createAction/page.tsx
+++ src/app/(general)/roadmap/[roadmapId]/goal/[goalId]/createAction/page.tsx
@@ -2,14 +2,18 @@ import getUserGroups from "@/functions/getUserGroups"
 import { getSessionData } from "@/lib/session"
 import { cookies } from "next/headers"
 import ActionForm from "./actionForm"
+import getOneRoadmap from "@/functions/getOneRoadmap"
 
-export default async function Page({ params }: { params: { roadmapId: string, goalId: string }}) {
+export default async function Page({ params }: { params: { roadmapId: string, goalId: string } }) {
   let session = await getSessionData(cookies())
   let userGroups: string[] = [...(await getUserGroups(session.user?.id!)), 'Public']
 
+  let roadmap = await getOneRoadmap(params.roadmapId)
+  let goal = roadmap?.goals.find(goal => goal.id === params.goalId)
+
   return (
     <>
-      <h1>Skapa åtgärd</h1>
+      <h1>Skapa ny åtgärd {roadmap ? `under målbanan "${goal?.name || "namn saknas"}" i "${roadmap.name}"` : ""}</h1>
       <ActionForm roadmapId={params.roadmapId} goalId={params.goalId} userGroups={userGroups} />
     </>
   )
