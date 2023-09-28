f8d7a1e2e30fb4eb197933006b08c416fb5833b2
Leon-Loov <leon.loov@outlook.com>
2023-09-28 11:36:11 +0200

    Remove /admin folder, as it's not used. Might reimplement later

--- src/app/(general)/admin/page.tsx
+++
@@ -1,8 +0,0 @@
-import { getSessionData } from '@/lib/session'
-import { cookies } from 'next/headers'
-
-export default async function Page() {
-  const { user } = await getSessionData(cookies())
-
-  return <h1>Hello {user?.username}!</h1>
-}
--- src/middleware.ts
+++ src/middleware.ts
@@ -5,12 +5,6 @@ export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
   const session = await getSession(req, res)
 
-  // Example of restricting access to specific paths/folders
-  if (req.nextUrl.pathname.startsWith('/admin')) {
-    if (!session.user) return new NextResponse(null, { status: 401 })
-    if (session.user.isLoggedIn !== true) return new NextResponse(null, { status: 403 })
-  }
-
   // Redirect away from login page if already logged in
   if (req.nextUrl.pathname.startsWith('/login')) {
     if (session.user?.isLoggedIn === true) {
