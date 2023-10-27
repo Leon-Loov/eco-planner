80c4a73ed68e05aa2dd2ac3e344305dc99c757e8
Leon-Loov <leon.loov@outlook.com>
2023-10-27 18:50:01 +0200

    Add route for editing actions
    Will probably need edits later, I haven't checked the code yet
    
    Signed-off-by: Leon-Loov <leon.loov@outlook.com>

--- src/app/api/createAction/route.ts
+++ src/app/api/createAction/route.ts
@@ -104,3 +104,85 @@ export async function POST(request: NextRequest) {
     { status: 500 }
   );
 }
+
+export async function PUT(request: NextRequest) {
+  const response = new Response();
+  const session = await getSession(request, response);
+
+  let action: ActionInput & { actionId: string } = await request.json();
+
+  // Validate request body
+  if (!action.actionId || !action.name) {
+    return createResponse(
+      response,
+      JSON.stringify({ message: 'Missing required input parameters' }),
+      { status: 400 }
+    );
+  }
+
+  // Validate session
+  if (!session.user?.isLoggedIn) {
+    return createResponse(
+      response,
+      JSON.stringify({ message: 'Unauthorized' }),
+      { status: 401 }
+    );
+  }
+
+  // Create lists of UUIDs for linking
+  let editors: { username: string }[] = [];
+  for (let name of action.editors || []) {
+    editors.push({ username: name });
+  }
+
+  let viewers: { username: string }[] = [];
+  for (let name of action.viewers || []) {
+    viewers.push({ username: name });
+  }
+
+  let editGroups: { name: string }[] = [];
+  for (let name of action.editGroups || []) {
+    editGroups.push({ name: name });
+  }
+
+  let viewGroups: { name: string }[] = [];
+  for (let name of action.viewGroups || []) {
+    viewGroups.push({ name: name });
+  }
+
+  // Update the action
+  try {
+    let updatedAction = await prisma.action.update({
+      where: {
+        id: action.actionId
+      },
+      data: {
+        name: action.name,
+        description: action.description,
+        costEfficiency: action.costEfficiency,
+        expectedOutcome: action.expectedOutcome,
+        startYear: action.startYear,
+        endYear: action.endYear,
+        projectManager: action.projectManager,
+        relevantActors: action.relevantActors,
+        editors: { set: editors },
+        viewers: { set: viewers },
+        editGroups: { set: editGroups },
+        viewGroups: { set: viewGroups },
+      }
+    });
+    // Return the new action's ID if successful
+    return createResponse(
+      response,
+      JSON.stringify({ message: 'Action updated', id: updatedAction.id }),
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    return createResponse(
+      response,
+      JSON.stringify({ message: "Internal server error" }),
+      { status: 500 }
+    );
+  }
+}
