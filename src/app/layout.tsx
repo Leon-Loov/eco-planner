e87eda96b0340c85fe532f9aa5359f268f5157c9
Leon-Loov <leon.loov@outlook.com>
2023-11-08 14:14:05 +0100

    Fix breadcrumbs
    Since it can't receive params from further down the tree, we're instead fetching all roadmaps all the time
    
    Signed-off-by: Leon-Loov <leon.loov@outlook.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,32 +3,27 @@ import { Header } from '@/components/header/header'
 import { GlobalContextProvider } from "./context/store"
 import getOneRoadmap from '@/functions/getOneRoadmap';
 import Breadcrumb from '@/components/breadcrumbs/breadcrumb2';
+import getRoadmaps from '@/functions/getRoadmaps';
 
 export default async function RootLayout({
   children,
-  params,
 }: {
   children: React.ReactNode,
-  params: {
-    roadmapId?: string,
-    goalId?: string,
-    actionId?: string,
-  }
 }) {
   // If there are params, get the relevant objects
-  let roadmap = params.roadmapId ? await getOneRoadmap(params.roadmapId) : null;
-  let goal = params.goalId && roadmap ? roadmap.goals.find(goal => goal.id === params.goalId) ?? null : null;
-  let action = params.actionId && goal ? goal.actions.find(action => action.id === params.actionId) ?? null : null;
+  let roadmaps = await getRoadmaps()
+  let goals = roadmaps.flatMap(roadmap => roadmap.goals)
+  let actions = goals.flatMap(goal => goal.actions)
 
   // Filter out nulls
-  let relevantObjects = [roadmap, goal, action].filter(object => object != null) as { id: string; name?: string | null | undefined; indicatorParameter?: string | null | undefined; }[]
+  let objects = [...roadmaps, ...goals, ...actions].filter(object => object != null) as { id: string; name?: string | null | undefined; indicatorParameter?: string | null | undefined; }[]
 
   return (
     <html lang="en">
       <body>
         <Header />
         <div className='layout-main'>
-          <Breadcrumb relevantObjects={relevantObjects} />
+          <Breadcrumb relevantObjects={objects} />
           <GlobalContextProvider>
             {children}
           </GlobalContextProvider>
