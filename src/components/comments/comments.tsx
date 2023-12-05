a0ad72b2172c5ead99f7480c91af0a49aa47e71e
Axelgustavschnurer <axel.schnurer@gmail.com>
2023-12-05 11:34:46 +0100

    Work on comment function

--- src/components/comments/comments.tsx
+++ src/components/comments/comments.tsx
@@ -2,6 +2,8 @@
 
 import { commentSorter } from "@/lib/sorters";
 import { Comment } from "@prisma/client";
+import styles from './comments.module.css'
+import { ChangeEvent, useState } from "react";
 
 export default function Comments({ comments, objectId }: { comments?: (Comment & { author: { id: string, username: string } })[], objectId: string }) {
   async function handleSubmit(event: React.ChangeEvent<HTMLFormElement>) {
@@ -33,22 +35,42 @@ export default function Comments({ comments, objectId }: { comments?: (Comment &
 
   // Sort comments by date
   comments?.sort(commentSorter);
+  const [editedContent, setEditedContent] = useState('Editable content');
+  const handleInput = (event: ChangeEvent<HTMLSpanElement>) => {
+    setEditedContent(event.target.innerText);
+    console.log(editedContent)
+  };
 
   return (
     <>
       <h2>Kommentarer</h2>
+      <form onSubmit={handleSubmit}>
+        <span className={styles.textarea} role="textbox" contentEditable aria-placeholder="Skriv Kommentar" onInput={handleInput} onBlur={handleInput} ></span>
+        <input type="hidden" name="comment" id="comment" value={editedContent} />
+        <div className="display-flex justify-content-flex-end gap-50 padding-y-50">
+          {/*
+          <button className={`${styles.button} ${styles.cancel}`}>Avbryt</button>
+          <button className={`${styles.button} ${styles.comment}`} >Kommentera</button>
+           */}
+          <input type="submit" value="Skicka" className="call-to-action-primary" style={{height: "100%"}} />
+
+        </div>
+      </form>
+      {/*
+      <form onSubmit={handleSubmit}>
+        <div className="display-flex gap-100">
+          <input type="text" name="comment" id="comment" style={{borderRadius: "3px", border: "none", padding: "1em"}} />
+          <input type="reset" value="reset"  style={{height: "100%"}} />
+          <input type="submit" value="Skicka" className="call-to-action-primary" style={{height: "100%"}} />
+        </div>
+      </form>
+      */}
       {comments?.map((comment) => (
         <div key={comment.id}>
           <p><b>{comment.author.username}</b> <i>{new Date(comment.createdAt).toLocaleString('sv-SE')}</i></p>
           <p>{comment.commentText}</p>
         </div>
       ))}
-      <form onSubmit={handleSubmit}>
-        <div className="display-flex gap-100">
-          <input type="text" name="comment" id="comment" style={{borderRadius: "3px", border: "none", padding: "1em"}} />
-          <input type="submit" value="Skicka" className="call-to-action-primary" style={{height: "100%"}} />
-        </div>
-      </form>
     </>
   )
 }
