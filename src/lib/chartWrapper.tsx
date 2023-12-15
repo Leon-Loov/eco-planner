6317d6cc9df8a260573a42c6161dfe299cbaec80
Leon-Loov <leon.loov@outlook.com>
2023-12-15 14:55:16 +0100

    Allow display of small numbers in floatSmoother
    Signed-off-by: Leon-Loov <leon.loov@outlook.com>

--- src/lib/chartWrapper.tsx
+++ src/lib/chartWrapper.tsx
@@ -63,5 +63,5 @@ export default function WrappedChart({
  * Declared as a client-side function so it can be used in `WrappedChart`.
  */
 export function floatSmoother(value: number) {
-  return value?.toLocaleString('sv-SE')
+  return value?.toLocaleString('sv-SE', { maximumFractionDigits: 10 })
 }
