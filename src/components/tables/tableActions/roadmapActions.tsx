2f27ba14813a794d9c40ab1db666767b7475d100
Axel <axel.schnurer@gmail.com>
2024-01-09 13:20:28 +0100

    Fix buttons

--- src/components/tables/tableActions/roadmapActions.tsx
+++ src/components/tables/tableActions/roadmapActions.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import Image from "next/image";
 import styles from './tableActions.module.css' with { type: "css" }
 import Link from "next/link";
+import { useState } from "react";
 
 //TODO: Close menu when pressing anywhere, also add close button
 
@@ -8,29 +11,48 @@ export function RoadmapActionButton(
   {
     addGoalHref,
     editHref,
-    roadmapName,
+    id,
   }: {
     addGoalHref: string,
     editHref: string,
-    roadmapName: string
+    id: string
   }) {
+
+  const [menu, setMenu] = useState(false)
+
+  const openMenu = () => {  
+    if (menu) {
+      setMenu(false)
+      return
+    }
+    setMenu(true)
+  }
 
   return (
     <>
       <div className={`${styles.actionButton} display-flex`}>
-        <input type="radio" name="openMenu" aria-label={`Redigera färdplan ${roadmapName}`}/>
-        <Image src='/icons/dotsVertical.svg' width={24} height={24} alt=""></Image>
-        <nav className={styles.menu}>
-          <Link href={addGoalHref} className={styles.menuAction}>
-            <Image src='/icons/plus-light.svg' alt="" width={24} height={24} />
-            <span>Lägg till målbana</span> 
-          </Link>
-          <Link href={editHref} className={styles.menuAction}>
-            <Image src='/icons/edit.svg' alt="" width={24} height={24} />
-            <span>Redigera färdplan</span>
-          </Link>
-          {/* <p>Ta bort färdplan</p> */}
-        </nav>
+        <button onClick={openMenu} className={styles.button}>
+          <Image src='/icons/dotsVertical.svg' width={24} height={24} alt=""></Image>
+        </button> {/* Button should open menu */}
+        {menu ?
+          <nav className={styles.menu} id={`${id}-menu`}>
+            <div className={`display-flex align-items-center justify-content-space-between ${styles.menuHeading}`}>
+              <strong>Redigera</strong>
+              <button onClick={openMenu} className={styles.button} >
+                <Image src='/icons/close.svg' alt="" width={18} height={18} />
+              </button>
+            </div>
+            <Link href={addGoalHref} className={styles.menuAction}>
+              <span>Lägg till målbana</span> 
+              <Image src='/icons/plus-light.svg' alt="" width={24} height={24} />
+            </Link>
+            <Link href={editHref} className={styles.menuAction}>
+              <span>Redigera färdplan</span>
+              <Image src='/icons/edit.svg' alt="" width={24} height={24} />
+            </Link>
+            {/* <p>Ta bort färdplan</p> */}
+          </nav>
+        : null }
       </div>
     </>
   )
