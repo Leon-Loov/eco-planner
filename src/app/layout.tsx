d026fe0bc1ea3cda045dca8171dcef002295001e
Axel <axel.schnurer@gmail.com>
2024-01-25 14:07:48 +0100

    Fix content width

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -81,7 +81,7 @@ export default async function RootLayout({
       <body>
         <div className='display-flex gap-100 padding-100'>
           <Header />
-          <div style={{minWidth: '0'}}>
+          <div className='flex-grow-100' style={{minWidth: '0'}}>
             <Breadcrumb relevantObjects={objects} />
             <GlobalContextProvider>
               {children}
