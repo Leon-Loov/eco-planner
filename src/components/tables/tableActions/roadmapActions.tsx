b2830e4c2e6bcc0d0fc3ef9e5c23002211bfd689
Leon-Loov <leon.loov@outlook.com>
2024-01-25 13:33:54 +0100

    Refactor roadmapActions to use dialog elements

--- src/components/tables/tableActions/roadmapActions.tsx
+++ src/components/tables/tableActions/roadmapActions.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import styles from './tableActions.module.css' with { type: "css" }
 import Link from "next/link";
-import { useState } from "react";
+import { useRef } from "react";
 
 //TODO: Close menu when pressing anywhere, also add close button
 
@@ -20,14 +20,25 @@ export function RoadmapActionButton(
     tableName: string,
   }) {
 
-  const [menu, setMenu] = useState(false)
+  let menu = useRef<HTMLDialogElement | null>(null);
 
-  const openMenu = () => {  
-    if (menu) {
-      setMenu(false)
-      return
+  const openMenu = () => {
+    menu.current?.show();
+  }
+
+  const closeMenu = (e: React.FocusEvent<HTMLDialogElement> | React.MouseEvent<HTMLButtonElement> | React.KeyboardEvent<HTMLDialogElement>) => {
+    // Don't close if focus stays within menu
+    if (e.type === 'blur') {
+      e = e as React.FocusEvent<HTMLDialogElement>
+      if (menu.current?.contains(e.relatedTarget as Node) || menu.current === e.relatedTarget) {
+        return;
+      }
     }
-    setMenu(true)
+    // Don't close if non-escape key is pressed
+    if ("key" in e && e.key !== 'Escape' && e.key !== 'Esc') {
+      return;
+    }
+    menu.current?.close();
   }
 
   return (
@@ -35,33 +46,29 @@ export function RoadmapActionButton(
       <div className={`${styles.actionButton} display-flex`}>
         <button onClick={openMenu} className={styles.button}>
           <Image src='/icons/dotsVertical.svg' width={24} height={24} alt=""></Image>
-        </button> {/* Button should open menu */}
-        {menu ?
-          <>
-            <nav className={styles.menu} id={`${id}-menu`}>
-              <div className={`display-flex align-items-center justify-content-space-between ${styles.menuHeading}`}>
-              <Link href={`/roadmap/${id}`} className={styles.menuHeadingTitle}>{tableName}</Link>
-                <button onClick={openMenu} className={styles.button} >
-                  <Image src='/icons/close.svg' alt="" width={18} height={18} />
-                </button>
-              </div>
-              <Link href={addGoalHref} className={styles.menuAction}>
-                <span>Ny målbana</span> 
-                <Image src='/icons/plus-light.svg' alt="" width={24} height={24} className={styles.actionImage} />
-              </Link>
-              <Link href={editHref} className={styles.menuAction}>
-                <span>Redigera färdplan</span>
-                <Image src='/icons/edit.svg' alt="" width={24} height={24} className={styles.actionImage} />
-              </Link>
-              {/*
-                <Link href={editHref} className={styles.menuAction}>
-                  <span>Radera färdplan</span>
-                  <Image src='/icons/delete.svg' alt="" width={24} height={24} className={styles.actionImage} />
-                </Link> 
-              */}
-            </nav>
-          </>
-          : null }
+        </button>
+        <dialog className={styles.menu} id={`${id}-menu`} onBlur={closeMenu} ref={menu} onKeyUp={closeMenu}>
+          <div className={`display-flex align-items-center justify-content-space-between ${styles.menuHeading}`}>
+            <Link href={`/roadmap/${id}`} className={styles.menuHeadingTitle}>{tableName}</Link>
+            <button onClick={closeMenu} className={styles.button} autoFocus >
+              <Image src='/icons/close.svg' alt="" width={18} height={18} />
+            </button>
+          </div>
+          <Link href={addGoalHref} className={styles.menuAction}>
+            <span>Ny målbana</span>
+            <Image src='/icons/plus-light.svg' alt="" width={24} height={24} className={styles.actionImage} />
+          </Link>
+          <Link href={editHref} className={styles.menuAction}>
+            <span>Redigera färdplan</span>
+            <Image src='/icons/edit.svg' alt="" width={24} height={24} className={styles.actionImage} />
+          </Link>
+          {/*
+            <Link href={editHref} className={styles.menuAction}>
+              <span>Radera färdplan</span>
+              <Image src='/icons/delete.svg' alt="" width={24} height={24} className={styles.actionImage} />
+            </Link> 
+          */}
+        </dialog>
       </div>
     </>
   )
