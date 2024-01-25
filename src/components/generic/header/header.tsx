0fba500fb9825ee5a60ede1d7a2f4449100bc933
Axel <axel.schnurer@gmail.com>
2024-01-25 16:34:35 +0100

    Update navigation appearance

--- src/components/generic/header/header.tsx
+++ src/components/generic/header/header.tsx
@@ -10,35 +10,39 @@ export async function Header() {
   const { user } = await getSessionData(cookies())
   return <>
     <aside className={styles.header}>
-      <div className={`display-flex flex-direction-column justify-content-space-between`}>
+      <div className={`display-flex flex-direction-column justify-content-space-between`} style={{height: '100%'}}>
         <div className={styles.menuToggleContainer}>
           <input type="checkbox" className={styles.menuToggle} />
           <Image src='/icons/menu.svg' alt='Toggle menu' width='24' height='24' /> 
         </div>
         <nav>
-          <Link href="/" className={styles.headerLink}>
-            <Image src='/icons/home.svg' alt='Hem' width={24} height={24} /> 
-            Hem 
-          </Link>
-          <Link href="/info" className={styles.headerLink}>
-            <Image src='/icons/info.svg' alt='Information' width={24} height={24} /> 
-            Information
-          </Link>
-          { // Link to login and signup if not logged in
-            !user?.isLoggedIn &&
-            <>
-              <PrimaryLink href='./signup'>Skapa Konto</PrimaryLink>
-              <SecondaryLink href='./login'>Logga In</SecondaryLink>
-            </>
-          }
-          { // Link to admin page and a logout button if logged in
-            user?.isLoggedIn &&
-            <>
-              {/* Admin pages don't currently exist */}
-              {/* <PrimaryLink href="./admin">To Admin Page</PrimaryLink> */}
-              <LogoutButton />
-            </>
-          }
+          <div>
+            <Link href="/" className={styles.headerLink}>
+              <Image src='/icons/home.svg' alt='Hem' width={24} height={24} /> 
+              Hem 
+            </Link>
+            <Link href="/info" className={styles.headerLink}>
+              <Image src='/icons/info.svg' alt='Information' width={24} height={24} /> 
+              Information
+            </Link>
+          </div>
+          <div style={{display: 'flex', flexDirection: 'column', gap: '.5rem'}}>
+            { // Link to login and signup if not logged in
+              !user?.isLoggedIn &&
+              <>
+                <PrimaryLink href='./signup'>Skapa Konto</PrimaryLink>
+                <SecondaryLink href='./login'>Logga In</SecondaryLink>
+              </>
+            }
+            { // Link to admin page and a logout button if logged in
+              user?.isLoggedIn &&
+              <>
+                {/* Admin pages don't currently exist */}
+                {/* <PrimaryLink href="./admin">To Admin Page</PrimaryLink> */}
+                <LogoutButton />
+              </>
+            }
+          </div>
         </nav>
       </div>
     </aside>
