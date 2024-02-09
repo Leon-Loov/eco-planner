79a423fcccb00a527abdf1a0df62983825392c97
Axelgustavschnurer <axel.schnurer@gmail.com>
2024-02-09 11:56:00 +0100

    Style metaRoadmapForm

--- src/components/forms/metaRoadmapForm/metaRoadmapForm.tsx
+++ src/components/forms/metaRoadmapForm/metaRoadmapForm.tsx
@@ -105,7 +105,7 @@ export default function MetaRoadmapForm({
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="action-form" >
         {/* This hidden submit button prevents submitting by pressing enter, this avoids accidental submission when adding new entries in AccessSelector (for example, when pressing enter to add someone to the list of editors) */}
         <input type="submit" disabled={true} style={{ display: 'none' }} aria-hidden={true} />
 
@@ -129,9 +129,11 @@ export default function MetaRoadmapForm({
         </select>
 
         <label htmlFor="actor">Aktör för färdplanen</label>
-        <input list="actors" id="actor" name="actor" defaultValue={currentRoadmap?.actor ?? undefined} />
+        <input list="actors" id="actor" name="actor" type="text" defaultValue={currentRoadmap?.actor ?? undefined} />
 
-        <LinkInput />
+        <div className="margin-y-300">
+          <LinkInput />
+        </div>
 
         { // Only show the access selector if a new roadmap is being created, the user is an admin, or the user is the author of the roadmap
           (!currentRoadmap || user?.isAdmin || user?.id === currentRoadmap.authorId) &&
@@ -157,7 +159,7 @@ export default function MetaRoadmapForm({
           }
         </select>
         {/* Add copy of RoadmapForm? Only if we decide to include it immediately rather than redirecting to it */}
-        <input type="submit" value={currentRoadmap ? "Spara" : "Skapa färdplan"} className="call-to-action-primary" />
+        <input type="submit" value={currentRoadmap ? "Spara" : "Skapa färdplan"} className="call-to-action-primary margin-y-100 " />
       </form>
 
       <datalist id="actors">
