b8aec4434b971ba082ebfe78a8559383b224b0e7
Leon-Loov <leon.loov@outlook.com>
2024-02-13 10:37:54 +0100

    Use absolute hrefs in header

--- src/components/generic/header/header.tsx
+++ src/components/generic/header/header.tsx
@@ -10,35 +10,35 @@ export async function Header() {
   const { user } = await getSessionData(cookies())
   return <>
     <aside className={styles.header}>
-      <div className={`display-flex flex-direction-column justify-content-space-between`} style={{height: '100%'}}>
+      <div className={`display-flex flex-direction-column justify-content-space-between`} style={{ height: '100%' }}>
         <div className={styles.menuToggleContainer}>
           <input type="checkbox" className={styles.menuToggle} />
-          <Image src='/icons/menu.svg' alt='Toggle menu' width='24' height='24' /> 
+          <Image src='/icons/menu.svg' alt='Toggle menu' width='24' height='24' />
         </div>
         <nav>
           <div>
             <Link href="/" className={styles.headerLink}>
-              <Image src='/icons/home.svg' alt='Hem' width={24} height={24} /> 
-              Hem 
+              <Image src='/icons/home.svg' alt='Hem' width={24} height={24} />
+              Hem
             </Link>
             <Link href="/info" className={styles.headerLink}>
-              <Image src='/icons/info.svg' alt='Information' width={24} height={24} /> 
+              <Image src='/icons/info.svg' alt='Information' width={24} height={24} />
               Information
             </Link>
           </div>
-          <div style={{display: 'flex', flexDirection: 'column', gap: '.5rem'}}>
+          <div style={{ display: 'flex', flexDirection: 'column', gap: '.5rem' }}>
             { // Link to login and signup if not logged in
               !user?.isLoggedIn &&
               <>
-                <PrimaryLink href='./signup'>Skapa Konto</PrimaryLink>
-                <SecondaryLink href='./login'>Logga In</SecondaryLink>
+                <PrimaryLink href='/signup'>Skapa Konto</PrimaryLink>
+                <SecondaryLink href='/login'>Logga In</SecondaryLink>
               </>
             }
             { // Link to admin page and a logout button if logged in
               user?.isLoggedIn &&
               <>
                 {/* Admin pages don't currently exist */}
-                {/* <PrimaryLink href="./admin">To Admin Page</PrimaryLink> */}
+                {/* <PrimaryLink href="/admin">To Admin Page</PrimaryLink> */}
                 <LogoutButton />
               </>
             }
@@ -48,4 +48,3 @@ export async function Header() {
     </aside>
   </>
 }
-                
