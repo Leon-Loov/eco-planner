d1fada28f32c316e09268fd0ed992bff05f53837
Axelgustavschnurer <axel.schnurer@gmail.com>
2024-02-18 20:56:48 +0100

    Fix bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,7 @@ export default async function Page() {
   return <>
     <div style={{ width: '100%', height: '350px', margin: '1.5rem 0', }}>
       <AttributedImage src="/images/solarpanels.jpg" alt="" borderRadius=".5em">
-        <div className="flex" style={{background: 'linear-gradient(180deg, transparent, rgba(0, 0, 0, .75)', height: '100%', alignItems: 'flex-end', padding: '1rem', borderRadius: '.5rem'}}>
+        <div className="flex" style={{background: 'linear-gradient(180deg, transparent, rgba(0, 0, 0, .75))', height: '100%', alignItems: 'flex-end', padding: '1rem', borderRadius: '.5rem'}}>
           <div>
             <h1 style={{marginBottom: '.5rem'}}>Färdplaner</h1>
             <p style={{color: 'white', margin: '0'}}>Photo by <a href="https://unsplash.com/@markusspiske?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" style={{color: 'white'}}>Markus Spiske</a> on <a href="https://unsplash.com/photos/white-and-blue-solar-panels-pwFr_1SUXRo?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" style={{color: 'white'}}>Unsplash</a></p>
