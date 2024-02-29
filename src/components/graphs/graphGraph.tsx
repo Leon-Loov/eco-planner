c22daa4828faf57031e3b2761c482b8c458bff41
Leon-Loov <leon.loov@outlook.com>
2024-02-29 12:35:40 +0100

    Refactor graphGraph component and graphSelector component
    Signed-off-by: Leon-Loov <leon.loov@outlook.com>

--- src/components/graphs/graphGraph.tsx
+++ src/components/graphs/graphGraph.tsx
@@ -1,57 +1,73 @@
 "use client"
 
-import { useGlobalContext } from "@/app/context/store";
 import MainDeltaGraph from "./mainDeltaGraph";
 import MainGraph from "./mainGraph";
 import MainRelativeGraph from "./mainRelativeGraph";
 import { DataSeries, Goal } from "@prisma/client";
 import GraphSelector from "./graphselector/graphSelector";
+import { useEffect, useState } from "react";
+import { getLocalStorage } from "@/functions/localStorage";
+
+export enum GraphType {
+  Main = "MAIN",
+  Relative = "RELATIVE",
+  Delta = "DELTA",
+}
 
 export default function GraphGraph({
   goal,
   nationalGoal,
-}:
-  {
-    goal: Goal & { dataSeries: DataSeries | null },
-    nationalGoal: Goal & { dataSeries: DataSeries | null } | null,
-  }) {
-  const { graphType } = useGlobalContext();
+}: {
+  goal: Goal & { dataSeries: DataSeries | null },
+  nationalGoal: Goal & { dataSeries: DataSeries | null } | null,
+}) {
+  const [graphType, setGraphType] = useState<GraphType | "">("");
 
-  return (
-    <div>
-      { // TODO: Make a proper toggle
-        graphType == 'mainGraph' ?
-          <>
-            <h2 className="display-flex align-items-center justify-content-space-between">Målbana
-              <nav className="display-flex align-items-center gap-50">
-                <GraphSelector goal={goal} />
-              </nav>
-            </h2>
-            <MainGraph goal={goal} nationalGoal={nationalGoal} />
-          </> : null
-      }
-      {
-        graphType == 'mainRelativeGraph' ?
-          <>
-            <h2 className="display-flex align-items-center justify-content-space-between">Procent relativt basår
-              <nav className="display-flex align-items-center gap-50">
-                <GraphSelector goal={goal} />
-              </nav>
-            </h2>
-            <MainRelativeGraph goal={goal} nationalGoal={nationalGoal} />
-          </> : null
-      }
-      {
-        graphType == 'mainDeltaGraph' ?
-          <>
-            <h2 className="display-flex align-items-center justify-content-space-between">Årlig förändring
-              <nav className="display-flex align-items-center gap-50">
-                <GraphSelector goal={goal} />
-              </nav>
-            </h2>
-            <MainDeltaGraph goal={goal} nationalGoal={nationalGoal} />
-          </> : null
-      }
-    </div>
-  )
+  useEffect(() => {
+    const storedGraphType = getLocalStorage(goal.id + "_graphType");
+    if (Object.values(GraphType).includes(storedGraphType)) {
+      setGraphType(storedGraphType);
+    }
+    else {
+      // Default to main graph
+      storedGraphType != null && console.log("Invalid graph type in storage, defaulting to main graph.");
+      setGraphType(GraphType.Main);
+    }
+  }, [goal.id]);
+
+  function graphSwitch(graphType: string) {
+    switch (graphType) {
+      case GraphType.Main:
+        return <div>
+          <h2 className="display-flex align-items-center justify-content-space-between">Målbana
+            <nav className="display-flex align-items-center gap-50">
+              <GraphSelector goal={goal} setter={setGraphType} />
+            </nav>
+          </h2>
+          <MainGraph goal={goal} nationalGoal={nationalGoal} />
+        </div>;
+      case GraphType.Relative:
+        return <div>
+          <h2 className="display-flex align-items-center justify-content-space-between">Procent relativt basår
+            <nav className="display-flex align-items-center gap-50">
+              <GraphSelector goal={goal} setter={setGraphType} />
+            </nav>
+          </h2>
+          <MainRelativeGraph goal={goal} nationalGoal={nationalGoal} />
+        </div>;
+      case GraphType.Delta:
+        return <div>
+          <h2 className="display-flex align-items-center justify-content-space-between">Årlig förändring
+            <nav className="display-flex align-items-center gap-50">
+              <GraphSelector goal={goal} setter={setGraphType} />
+            </nav>
+          </h2>
+          <MainDeltaGraph goal={goal} nationalGoal={nationalGoal} />
+        </div>;
+      default:
+        return graphSwitch(GraphType.Main);
+    }
+  };
+
+  return graphSwitch(graphType);
 }
--- src/components/graphs/graphselector/graphSelector.tsx
+++ src/components/graphs/graphselector/graphSelector.tsx
@@ -1,26 +1,55 @@
 "use client";
 
-import { useGlobalContext } from "@/app/context/store";
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, Dispatch, SetStateAction, useEffect, useState } from 'react';
 import RadioImage from './radioImage';
 import { DataSeries, Goal } from "@prisma/client";
+import { GraphType } from "../graphGraph";
+import { getLocalStorage, setLocalStorage } from '@/functions/localStorage';
 
-export default function GraphSelector({ goal }: { goal: Goal & { dataSeries: DataSeries | null } }) {
-  const { graphType, setGraphType } = useGlobalContext();
+export default function GraphSelector({
+  goal,
+  setter,
+}: {
+  goal: Goal & { dataSeries: DataSeries | null },
+  setter?: Dispatch<SetStateAction<GraphType | "">>
+}) {
+  const [graphType, setGraphType] = useState("");
+
+  useEffect(() => {
+    const storedGraphType = getLocalStorage(goal.id + "_graphType");
+    if (Object.values(GraphType).includes(storedGraphType)) {
+      setGraphType(storedGraphType);
+    }
+    else {
+      // Default to main graph
+      storedGraphType != null && console.log("Invalid graph type in storage, defaulting to main graph.");
+      setGraphType(GraphType.Main);
+    }
+  }, [goal.id]);
 
   const handleRadioChange = (event: ChangeEvent<HTMLInputElement>) => {
     setGraphType(event.target.value);
+    setLocalStorage(goal.id + "_graphType", event.target.value);
+    if (setter) {
+      if (Object.values(GraphType).includes(event.target.value as GraphType)) {
+        setter(event.target.value as GraphType);
+      }
+      else {
+        console.log("Invalid graph type");
+        setter("");
+      }
+    }
   };
 
   // Set the selectedOption as the context value
   return (
     <>
-      <RadioImage value="mainGraph" src="/icons/chart.svg" name="graph" checked={graphType === 'mainGraph'} onChange={handleRadioChange} />
+      <RadioImage value={GraphType.Main} src="/icons/chart.svg" name="graph" checked={graphType == GraphType.Main} onChange={handleRadioChange} />
       { // Don't allow relative graph if the main graph is already percent or fraction
         goal.dataSeries?.unit.toLowerCase() != 'procent' && goal.dataSeries?.unit.toLowerCase() != 'andel' &&
-        <RadioImage value="mainRelativeGraph" src="/icons/chartChange.svg" name="graph" checked={graphType === 'mainRelativeGraph '} onChange={handleRadioChange} />
+        <RadioImage value={GraphType.Relative} src="/icons/chartChange.svg" name="graph" checked={graphType == GraphType.Relative} onChange={handleRadioChange} />
       }
-      <RadioImage value="mainDeltaGraph" src="/icons/delta.svg" name="graph" checked={graphType === 'mainDeltaGraph'} onChange={handleRadioChange} />
+      <RadioImage value={GraphType.Delta} src="/icons/delta.svg" name="graph" checked={graphType == GraphType.Delta} onChange={handleRadioChange} />
     </>
   );
 }
