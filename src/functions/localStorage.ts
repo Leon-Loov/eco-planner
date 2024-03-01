ed0f3ceeed9d9ea6e0b845d40bd8670da22b5e7e
Leon-Loov <leon.loov@outlook.com>
2024-03-01 16:33:02 +0100

    Add more storage functions

--- src/functions/localStorage.ts
+++ src/functions/localStorage.ts
@@ -12,6 +12,16 @@ export function setSessionStorage(key: string, value: any) {
   sessionStorage.setItem(key, JSON.stringify(value));
 }
 
+/** Stringifies `value` and stores it in localStorage under `key`. */
+export function setLocalStorage(key: string, value: any) {
+  if (!localStorage) {
+    console.error('No localStorage available');
+    return;
+  }
+
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
 /** Retrieves the value stored under `key` in sessionStorage, parsed as JSON. */
 export function getSessionStorage(key: string): any {
   if (!sessionStorage) {
@@ -26,6 +36,40 @@ export function getSessionStorage(key: string): any {
   return null;
 }
 
+/** Retrieves the value stored under `key` in localStorage, parsed as JSON. */
+export function getLocalStorage(key: string): any {
+  if (!localStorage) {
+    console.error('No localStorage available');
+    return null;
+  }
+
+  const value = localStorage.getItem(key);
+  if (value) {
+    return JSON.parse(value);
+  }
+  return null;
+}
+
+/** Retrieves all keys in sessionStorage. */
+export function getSessionStorageKeys() {
+  if (!sessionStorage) {
+    console.error('No sessionStorage available');
+    return null;
+  }
+
+  return Object.keys(sessionStorage);
+}
+
+/** Retrieves all keys in localStorage. */
+export function getLocalStorageKeys() {
+  if (!localStorage) {
+    console.error('No localStorage available');
+    return null;
+  }
+
+  return Object.keys(localStorage);
+}
+
 /** Removes the value stored under `key` in sessionStorage. */
 export function removeSessionStorage(key: string) {
   if (!sessionStorage) {
@@ -34,4 +78,14 @@ export function removeSessionStorage(key: string) {
   }
 
   sessionStorage.removeItem(key);
+}
+
+/** Removes the value stored under `key` in localStorage. */
+export function removeLocalStorage(key: string) {
+  if (!localStorage) {
+    console.error('No localStorage available');
+    return;
+  }
+
+  localStorage.removeItem(key);
 }
