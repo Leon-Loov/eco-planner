8ac212fcf1a708756b0551033776b90792010425
Axel <axel.schnurer@gmail.com>
2024-03-08 15:29:39 +0100

    Style frontpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,7 +59,7 @@ export default async function Page() {
       </AttributedImage>
     </div>
     <section>
-      <section className="margin-y-100 padding-y-100">
+      <section className="margin-y-100 padding-y-50" style={{borderBottom: '1px solid var(--gray-90)'}}>
         <label className="font-weight-bold margin-y-25 container-text">
           Sök färdplan
             <div className="margin-y-50 flex align-items-center gray-90 padding-50 smooth focusable">
@@ -78,7 +78,7 @@ export default async function Page() {
             </select>
           </label>
           <label className='flex align-items-center gap-50 padding-25 button smooth transparent'>
-            <span>Filtrera</span>
+            <span style={{lineHeight: '1'}}>Filtrera</span>
             <div className='position-relative grid place-items-center'> 
               <input type="checkbox" className="position-absolute width-100 height-100 hidden"/>
               <Image src="/icons/filter.svg" alt="" width="24" height="24" />
@@ -86,8 +86,8 @@ export default async function Page() {
           </label>
         </div>
       </section>
-      <section id="roadmapFilters" className="margin-y-100 padding-100 gray-90 rounded">
-        <p><b>Visa</b></p>
+      <section id="roadmapFilters" className="margin-y-200 padding-100 gray-90 rounded">
+        <b>Visa</b>
         <label className="flex align-items-center gap-25 margin-y-50">
           <input type="checkbox"/>
             Nationella färdplaner
