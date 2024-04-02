ad8ecb27420d1822e32933632bac6cac3f4e5e78
Axelgustavschnurer <axel.schnurer@gmail.com>
2024-04-02 16:15:10 +0200

    Style deletemodal

--- src/components/modals/confirmDelete.tsx
+++ src/components/modals/confirmDelete.tsx
@@ -3,6 +3,7 @@
 import formSubmitter from "@/functions/formSubmitter";
 import { useState } from "react";
 import { closeModal } from "@/components/modals/modalFunctions";
+import styles from './modals.module.css'
 
 export default function ConfirmDelete({
   modalRef,
@@ -24,13 +25,19 @@ export default function ConfirmDelete({
   };
 
   return (
-    <dialog ref={modalRef}>
-      <h2>Radera inlägg</h2>
-      <p>För att radera {targetName}, skriv in namnet i rutan nedan och klicka sedan {'"Radera"'}</p>
-      <input type="text" placeholder={targetName} />
-      <div>
-        <button type="button" onClick={() => closeModal(modalRef)}>Avbryt</button>
-        <button type="button" disabled={isLoading} onClick={handleDelete}>Radera</button>
+    <dialog ref={modalRef} className={styles.modal}>
+      <strong className="block" style={{fontSize: 'larger'}}>Radera inlägg</strong>
+      <p className="padding-y-100" style={{borderBlock: '2px solid var(--gray-90)'}}>
+        Är du säker på att du vill radera inlägg <strong>{targetName}</strong>? <br/>
+        Du kan ej ångra denna åtgärd senare.
+      </p>
+      <label className="block margin-y-75">
+        <strong>Skriv {targetName} för att bekräfta</strong>
+        <input className="margin-y-25" type="text" placeholder={targetName} />
+      </label>
+      <div className="display-flex justify-content-flex-end margin-top-75">
+        <button type="button" className="margin-x-25" style={{fontWeight: 500}} onClick={() => closeModal(modalRef)}>Avbryt</button>
+        <button type="button" className="margin-x-25 red color-purewhite" style={{fontWeight: 500}} disabled={isLoading} onClick={handleDelete}>Radera</button>
       </div>
     </dialog>
   );
