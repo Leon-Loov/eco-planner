13a17c038fb737b6053d48d3d1a4c7a446ce1c2b
Leon-Loov <leon.loov@outlook.com>
2024-04-02 17:15:58 +0200

    Add input validation to `confirmDelete`

--- src/components/modals/confirmDelete.tsx
+++ src/components/modals/confirmDelete.tsx
@@ -19,6 +19,10 @@ export default function ConfirmDelete({
   const [isLoading, setIsLoading] = useState(false);
 
   function handleDelete() {
+    // Check if the input matches the target name
+    if ((document.getElementById("delete-name-input") as HTMLInputElement)?.value !== targetName || !(document.getElementById("delete-name-input") as HTMLInputElement)?.value) {
+      return;
+    }
     setIsLoading(true);
     formSubmitter(targetUrl, JSON.stringify({ id: targetId }), "DELETE", setIsLoading)
     closeModal(modalRef);
@@ -26,19 +30,21 @@ export default function ConfirmDelete({
 
   return (
     <dialog ref={modalRef} className={styles.modal}>
-      <strong className="block" style={{fontSize: 'larger'}}>Radera inlägg</strong>
-      <p className="padding-y-100" style={{borderBlock: '2px solid var(--gray-90)'}}>
-        Är du säker på att du vill radera inlägg <strong>{targetName}</strong>? <br/>
-        Du kan ej ångra denna åtgärd senare.
-      </p>
-      <label className="block margin-y-75">
-        <strong>Skriv {targetName} för att bekräfta</strong>
-        <input className="margin-y-25" type="text" placeholder={targetName} />
-      </label>
-      <div className="display-flex justify-content-flex-end margin-top-75 gap-50">
-        <button type="button" style={{fontWeight: 500}} onClick={() => closeModal(modalRef)}>Avbryt</button>
-        <button type="button" className="red color-purewhite" style={{fontWeight: 500}} disabled={isLoading} onClick={handleDelete}>Radera</button>
-      </div>
+      <form onSubmit={handleDelete}>
+        <strong className="block" style={{ fontSize: 'larger' }}>Radera inlägg</strong>
+        <p className="padding-y-100" style={{ borderBlock: '2px solid var(--gray-90)' }}>
+          Är du säker på att du vill radera inlägget <strong>{targetName}</strong>? <br />
+          Du kan ej ångra denna åtgärd senare.
+        </p>
+        <label className="block margin-y-75">
+          Skriv <strong>{targetName}</strong> för att bekräfta
+          <input className="margin-y-25" type="text" placeholder={targetName} id="delete-name-input" required pattern={targetName} />
+        </label>
+        <div className="display-flex justify-content-flex-end margin-top-75 gap-50">
+          <button type="button" style={{ fontWeight: 500 }} onClick={() => closeModal(modalRef)}>Avbryt</button>
+          <button type="submit" className="red color-purewhite" style={{ fontWeight: 500 }} disabled={isLoading} onClick={handleDelete}>Radera</button>
+        </div>
+      </form>
     </dialog>
   );
 }
