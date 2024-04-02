a6741315d5224c496727cfa66273177b045e1d89
Axelgustavschnurer <axel.schnurer@gmail.com>
2024-04-02 16:25:32 +0200

    Fix margin

--- src/components/modals/confirmDelete.tsx
+++ src/components/modals/confirmDelete.tsx
@@ -35,9 +35,9 @@ export default function ConfirmDelete({
         <strong>Skriv {targetName} för att bekräfta</strong>
         <input className="margin-y-25" type="text" placeholder={targetName} />
       </label>
-      <div className="display-flex justify-content-flex-end margin-top-75">
-        <button type="button" className="margin-x-25" style={{fontWeight: 500}} onClick={() => closeModal(modalRef)}>Avbryt</button>
-        <button type="button" className="margin-x-25 red color-purewhite" style={{fontWeight: 500}} disabled={isLoading} onClick={handleDelete}>Radera</button>
+      <div className="display-flex justify-content-flex-end margin-top-75 gap-50">
+        <button type="button" style={{fontWeight: 500}} onClick={() => closeModal(modalRef)}>Avbryt</button>
+        <button type="button" className="red color-purewhite" style={{fontWeight: 500}} disabled={isLoading} onClick={handleDelete}>Radera</button>
       </div>
     </dialog>
   );
