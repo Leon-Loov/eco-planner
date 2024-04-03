cd8d5886127dfa7bff9ae1ec80116443dc2d1283
Axel <axel.schnurer@gmail.com>
2024-04-03 17:49:08 +0200

    Add formslider

--- src/components/forms/metaRoadmapForm/metaRoadmapForm.tsx
+++ src/components/forms/metaRoadmapForm/metaRoadmapForm.tsx
@@ -4,10 +4,11 @@ import countiesAndMunicipalities from "@/lib/countiesAndMunicipalities.json" wit
 import { Data } from "@/lib/session";
 import { AccessControlled, MetaRoadmapInput } from "@/types";
 import { MetaRoadmap, RoadmapType } from "@prisma/client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AccessSelector, { getAccessData } from "@/components/forms/accessSelector/accessSelector";
 import LinkInput, { getLinks } from "@/components/forms/linkInput/linkInput"
 import formSubmitter from "@/functions/formSubmitter";
+import styles from '../forms.module.css'
 
 export default function MetaRoadmapForm({
   user,
@@ -83,71 +84,106 @@ export default function MetaRoadmapForm({
     }
   }
 
+  const [transformIndex, setTransformIndex] = useState(1)
+
+  function doStuff() {
+    const aaaaa = document?.getElementById('form-sections')
+    let bbbbb = Array.from(aaaaa?.children || [])
+
+    if (transformIndex + 1 > bbbbb.length) {
+      return
+    }
+
+    bbbbb.forEach(element => {
+      const transformData = (element as HTMLElement).dataset.transform
+      if (transformData) {
+        let test = parseInt(transformData);
+        (element as HTMLElement).dataset.transform = (test - 100).toString();
+        let datasetTransform = (element as HTMLElement).dataset.transform;
+        (element as HTMLElement).style.transform = `translate(${datasetTransform}%, 0)`
+
+      }
+    })
+
+    setTransformIndex(transformIndex + 1)
+
+  }
+
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} >
         {/* This hidden submit button prevents submitting by pressing enter, this avoids accidental submission when adding new entries in AccessSelector (for example, when pressing enter to add someone to the list of editors) */}
         <input type="submit" disabled={true} style={{ display: 'none' }} aria-hidden={true} />
 
-        <label className="block margin-y-75">
-          Namn för den nya färdplanen
-          <input id="metaRoadmapName" name="metaRoadmapName" className="margin-y-25" type="text" defaultValue={currentRoadmap?.name ?? undefined} required />
-        </label>
+        <div id="form-sections" className={styles.formSlider}>
+          <section className="width-100" data-transform="0">
+            <label className="block margin-y-75">
+              Namn för den nya färdplanen
+              <input id="metaRoadmapName" name="metaRoadmapName" className="margin-y-25" type="text" defaultValue={currentRoadmap?.name ?? undefined} required />
+            </label>
 
-        <label className="block margin-y-75">
-          Beskrivning av färdplanen
-          <textarea className="block smooth margin-y-25" name="description" id="description" defaultValue={currentRoadmap?.description ?? undefined} required></textarea>
-        </label>
+            <label className="block margin-y-75">
+              Beskrivning av färdplanen
+              <textarea className="block smooth margin-y-25" name="description" id="description" defaultValue={currentRoadmap?.description ?? undefined} required></textarea>
+            </label>
+          </section>
 
-        <label className="block margin-y-75">
-          Typ av färdplan
-          <select className="block margin-y-25" name="type" id="type" defaultValue={currentRoadmap?.type ?? ""} required>
-            <option value="">Välj en typ</option>
-            {
-              Object.values(RoadmapType).map((value) => {
-                if (value == RoadmapType.NATIONAL && !user?.isAdmin) return null;
-                return (
-                  <option key={value} value={value}>{value in customRoadmapTypes ? customRoadmapTypes[value] : value}</option>
+          <section className="width-100" data-transform="0">
+            <label className="block margin-y-75">
+              Typ av färdplan
+              <select className="block margin-y-25" name="type" id="type" defaultValue={currentRoadmap?.type ?? ""} required>
+                <option value="">Välj en typ</option>
+                {
+                  Object.values(RoadmapType).map((value) => {
+                    if (value == RoadmapType.NATIONAL && !user?.isAdmin) return null;
+                    return (
+                      <option key={value} value={value}>{value in customRoadmapTypes ? customRoadmapTypes[value] : value}</option>
+                    )
+                  })
+                }
+              </select>
+            </label>
+
+            <label className="block margin-y-75">
+              Aktör för färdplanen
+              <input className="margin-y-25" list="actors" id="actor" name="actor" type="text" defaultValue={currentRoadmap?.actor ?? undefined} />
+            </label>
+          </section>
+
+          <section className="margin-y-300 width-100" data-transform="0">
+            <LinkInput />
+          </section>
+
+          { // Only show the access selector if a new roadmap is being created, the user is an admin, or the user is the author of the roadmap
+            (!currentRoadmap || user?.isAdmin || user?.id === currentRoadmap.authorId) &&
+            <section data-transform="0">
+              <AccessSelector groupOptions={userGroups} currentAccess={currentAccess} />
+            </section>
+          }
+
+          <section className="width-100" data-transform="0">
+            <label className="block margin-y-75">
+              Om denna färdplan har en annan färdplan den jobbar mot kan den väljas här
+              <select name="parentRoadmap" id="parentRoadmap" className="block margin-y-25" defaultValue={currentRoadmap?.parentRoadmapId ?? ""}>
+              <option value="">Ingen förälder vald</option>
+              {
+                !parentRoadmapOptions && currentRoadmap && currentRoadmap.parentRoadmapId && (
+                  <option value={currentRoadmap.parentRoadmapId} disabled>{currentRoadmap.parentRoadmapId}</option>
                 )
-              })
-            }
-          </select>
-          </label>
-
-        <label className="block margin-y-75">
-          Aktör för färdplanen
-          <input className="margin-y-25" list="actors" id="actor" name="actor" type="text" defaultValue={currentRoadmap?.actor ?? undefined} />
-        </label>
-
-        <div className="margin-y-300">
-          <LinkInput />
+              }
+              {
+                parentRoadmapOptions && parentRoadmapOptions.map((roadmap) => {
+                  return (
+                    <option key={roadmap.id} value={roadmap.id}>{roadmap.name}</option>
+                  )
+                })
+              }
+              </select>
+            </label>
+          </section>
         </div>
 
-        { // Only show the access selector if a new roadmap is being created, the user is an admin, or the user is the author of the roadmap
-          (!currentRoadmap || user?.isAdmin || user?.id === currentRoadmap.authorId) &&
-          <>
-            <AccessSelector groupOptions={userGroups} currentAccess={currentAccess} />
-          </>
-        }
-
-        <label className="block margin-y-75">
-          Om denna färdplan har en annan färdplan den jobbar mot kan den väljas här
-          <select name="parentRoadmap" id="parentRoadmap" className="block margin-y-25" defaultValue={currentRoadmap?.parentRoadmapId ?? ""}>
-          <option value="">Ingen förälder vald</option>
-          {
-            !parentRoadmapOptions && currentRoadmap && currentRoadmap.parentRoadmapId && (
-              <option value={currentRoadmap.parentRoadmapId} disabled>{currentRoadmap.parentRoadmapId}</option>
-            )
-          }
-          {
-            parentRoadmapOptions && parentRoadmapOptions.map((roadmap) => {
-              return (
-                <option key={roadmap.id} value={roadmap.id}>{roadmap.name}</option>
-              )
-            })
-          }
-        </select>
-      </label>
+        <button type="button" onClick={doStuff}>→</button>
 
         {/* Add copy of RoadmapForm? Only if we decide to include it immediately rather than redirecting to it */}
         <input type="submit" value={currentRoadmap ? "Spara" : "Skapa färdplan"} className="margin-y-100 seagreen color-purewhite" />
