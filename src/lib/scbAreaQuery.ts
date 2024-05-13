6d4468f702eeb92330742708edb35d3b56b4e2da
Leon-Loov <leon.loov@outlook.com>
2024-05-13 16:22:47 +0200

    Update population and area queries to use new version of PxWeb API

--- src/lib/scbAreaQuery.ts
+++ src/lib/scbAreaQuery.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import { unstable_cache } from "next/cache";
+import { getTableContent } from "./pxWeb/getTableContent";
 
 /**
  * Queries SCB for area data.
@@ -14,80 +15,20 @@ export default async function scbAreaQuery(areaCode: string, parentAreaCode?: st
 
 const getCachedQuery = unstable_cache(
   async (areaCode: string, parentAreaCode?: string) => {
-    const url = `https://api.scb.se/OV0104/v1/doris/sv/ssd/START/MI/MI0802/Areal2012NN`;
+    const selection = [
+      // Include parent area only if it exists
+      { variableCode: "Region", valueCodes: [areaCode, ...(parentAreaCode ? [parentAreaCode] : [])] },
+      { variableCode: "ArealTyp", valueCodes: ["01"] },
+      { variableCode: "ContentsCode", valueCodes: ["000001O3"] },
+      // Use the latest time period
+      { variableCode: "Tid", valueCodes: ["TOP(1)"] }
+    ];
 
-    // Get the latest time period, or default to 2023 (latest available as of 2024-02-01)
-    const time = await fetch(url).then((res) => { if (!res.ok) throw new Error; return res.json() }).then((data) => (data.variables[3].values as string[]).reverse()[0]).catch(() => { return null; }) ?? "2023";
+    const result = await getTableContent("TAB2946", selection, "SCB", "sv")
 
-    // See https://www.scb.se/contentassets/79c32c72783a4f67b202ad3189f921b9/api_beskrivning.pdf for more info (in Swedish) on the query format
-    // At https://www.statistikdatabasen.scb.se/pxweb/sv/ssd/ you can find a UI for building basic queries in Swedish
-    // Also available in English at https://www.statistikdatabasen.scb.se/pxweb/en/ssd/
-    const query = {
-      "query": [
-        {
-          "code": "Region",
-          "selection": {
-            "filter": "item",
-            "values": [
-              areaCode,
-              ...(parentAreaCode ? [parentAreaCode] : [])
-            ]
-          }
-        },
-        {
-          // Land area; total area including water is "04"
-          "code": "ArealTyp",
-          "selection": {
-            "filter": "item",
-            "values": [
-              "01"
-            ]
-          }
-        },
-        {
-          // Square kilometers (as opposed to hectares, "000001O4")
-          "code": "ContentsCode",
-          "selection": {
-            "filter": "item",
-            "values": [
-              "000001O3"
-            ]
-          }
-        },
-        {
-          "code": "Tid",
-          "selection": {
-            "filter": "item",
-            "values": [
-              time
-            ]
-          }
-        }
-      ],
-      "response": {
-        "format": "json"
-      }
-    };
+    if (!result) return null;
 
-    const response = await fetch(url, {
-      method: 'POST',
-      body: JSON.stringify(query)
-    }).catch((error) => {
-      console.log(error);
-      return;
-    });
-
-    // If the query fails, return null
-    if (!response || !response.ok) return null;
-
-    const data: {
-      columns: { code: string, text: string, type?: string, comment?: string }[],
-      comments?: { variable: string, value: string, comment?: string }[],
-      data: { key: string[], values: string[], comment?: string }[],
-      metadata: { infofile: string, updated: string, label: string, source: string, comment?: string }[],
-    } = await response.json();
-
-    const areaData = data.data;
+    const areaData = result.data;
     const area = areaData.find((data) => data.key[0] == areaCode)?.values[0];
     const parentArea = parentAreaCode && areaData.find((data) => data.key[0] == parentAreaCode)?.values[0];
 
