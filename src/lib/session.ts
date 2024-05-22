726062973c25da74317b13afe28b224fb72ebcc1
Leon-Loov <leon.loov@outlook.com>
2024-05-22 17:09:24 +0200

    Change login cookie to last one browser session rather than persisting for a certain amount of time

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,4 +1,4 @@
-import { IronSession, getIronSession } from "iron-session";
+import { IronSession, SessionOptions, getIronSession } from "iron-session";
 import { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
 
 // The information we store in our cookie
@@ -13,10 +13,13 @@ export interface Data {
 }
 
 // Config stuff for Iron-Session
-export const options = {
+export const options: SessionOptions = {
   password: process.env.IRON_SESSION_PASSWORD!,
   cookieName: "eco_planner",
   cookieOptions: {
+    // Undefined maxAge results in so called "session cookies", which are deleted when the browsing session ends.
+    // This results in GDPR-compliant login cookies (with a "remember me"-checkbox or similar we may set a maxAge and keep the cookie between sessions)
+    maxAge: undefined,
     // Uses https in production and http in development
     secure: process.env.NODE_ENV === "production",
   },
